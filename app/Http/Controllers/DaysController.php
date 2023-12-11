3268a83d8fd2fddbe37c9f725471686633310374
unknown <shmsheshan253@gmail.com>
2023-12-11 14:04:05 +0200

    news

---
+++ app/Http/Controllers/DaysController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\days;
+use App\Http\Requests\StoredaysRequest;
+use App\Http\Requests\UpdatedaysRequest;
+
+class DaysController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoredaysRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(days $days)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatedaysRequest $request, days $days)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(days $days)
+    {
+        //
+    }
+}
--- database/migrations/2023_12_11_115737_create_days_table.php
+++ database/migrations/2023_12_11_115737_create_days_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('exercise_programm', function (Blueprint $table) {
+        Schema::create('days', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('exercise_programm');
+        Schema::dropIfExists('days');
     }
 };

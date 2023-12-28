38a44942e52d794b2b45cd0c4fc01df31947f63a
ali007dev <ali007developer@gmail.com>
2023-12-28 10:22:45 +0300

    aa

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -16,7 +16,6 @@ class ImageSeeder extends Seeder
         ['userId' => '1','exerciseId'=>null,'image'=>'460838971.jpg'],
         ['userId' => '1','exerciseId'=>null,'image'=>'765871961.jpg'],
 
-<<<<<<< HEAD
         ['userId' => '2','exerciseId'=>null,'image'=>'919412255.jpg'],
         ['userId' => '2','exerciseId'=>null,'image'=>'626584726.jpg'],
 
@@ -29,26 +28,10 @@ class ImageSeeder extends Seeder
         ['userId' => '5','exerciseId'=>null,'image'=>'301136981.jpg'],
         ['userId' => '5','exerciseId'=>null,'image'=>'1903471181.jpg'],
 
+
        
-=======
-<<<<<<< HEAD
-        ['userId' => '2','exerciseId'=>null,'image'=>'694402835.jpg'],
-        ['userId' => '3','exerciseId'=>null,'image'=>'694402835.jpg'],
-        ['userId' => '4','exerciseId'=>null,'image'=>'694402835.jpg'],
-        ['userId' => '5','exerciseId'=>null,'image'=>'694402835.jpg'],
-        ['userId' => '3','exerciseId'=>null,'image'=>'694402835.jpg'],
-=======
-        ['userId' => '2','exerciseId'=>null,'image'=>'462778385.jpg'],
-        ['userId' => '3','exerciseId'=>null,'image'=>'462778385.jpg'],
-        ['userId' => '4','exerciseId'=>null,'image'=>'238432623.jpg'],
-        ['userId' => '5','exerciseId'=>null,'image'=>'238432623.jpg'],
-        ['userId' => '8','exerciseId'=>null,'image'=>'238432623.jpg'],
-        ['userId' => '6','exerciseId'=>null,'image'=>'238432623.jpg'],
-        ['userId' => '7','exerciseId'=>null,'image'=>'238432623.jpg'],
->>>>>>> 40bd5a4cae99d5eef1221cf4d598eaef9c6fd250
 
 
->>>>>>> 549af65618835444f8eaf028b92af7db557c9f7a
 
 
     ]);

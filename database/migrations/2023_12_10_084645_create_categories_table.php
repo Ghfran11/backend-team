0be7b179e54aeb3de19c16ff4ac6b22e08b101e6
Ghfran11 <shmsheshan253@gmail.com>
2024-02-25 11:51:14 +0300

    new

--- database/migrations/2023_12_10_084645_create_categories_table.php
+++ database/migrations/2023_12_10_084645_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description');
             $table->string('imageUrl')->nullable();
-            $table->enum('type',['Sport','food']);
+            $table->enum('type',['sport','food']);
             $table->timestamps();
         });
     }

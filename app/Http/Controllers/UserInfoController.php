09a4ea90772c6398c1f188e211f0b2d20132beb8
thalesMworkAcc <thalesmwork942@gmail.com>
2024-03-31 10:09:30 +0300

    user info

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -100,7 +100,7 @@ class UserInfoController extends Controller
             );
 
             if ($request->has('image')) {
-                $this->imageService->storeImage($request, Auth::id(), null,null);
+                $this->imageService->storeImage($request, Auth::id(), null, null);
             }
             return ResponseHelper::success($userInfo);
         } catch (\Exception $e) {
@@ -156,29 +156,4 @@ class UserInfoController extends Controller
         }
     }
 
-
-$gender = $request->gender;
-$weight = $request->weight;
-$waist_measurement = $request->waistMeasurement;
-$neck = $request->neck;
-$height = $request->height;
-$hip = $request->hip;
-
-
-$waist_inch = $waist_measurement / 2.54;
-$neck_inch = $neck / 2.54;
-$hip_inch = $hip / 2.54;
-
-
-if ($gender === 'male') {
-    $body_density = 1.0324 - 0.19077 * log10($waist_inch - $neck_inch) + 0.15456 * log10($height);
-} elseif ($gender === 'female') {
-    $body_density = 1.29579 - 0.35004 * log10($waist_inch + $hip_inch - $neck_inch) + 0.221 * log10($height);
-} else {
-
-
-
-$bfp = (495 / $body_density) - 450;
-
 }
-

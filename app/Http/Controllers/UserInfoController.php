e6e7ca361729dec2dd2bc3eaa8250c4b18c42b73
mazharalhayek12@gmail.com <mazharalhayek12@gmail.com>
2024-03-31 12:42:51 +0300

    user controller to service

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -87,7 +87,7 @@ class UserInfoController extends Controller
         try {
             $user = Auth::user();
 
-            $userInfo = $user->userInfo()->updateOrcreate(
+            $userInfo = $user->userInfo->updateOrcreate(
                 ['userId' => $user->id],
                 [
                     'gender' => $request->gender,
@@ -157,28 +157,8 @@ class UserInfoController extends Controller
     }
 
 
-$gender = $request->gender;
-$weight = $request->weight;
-$waist_measurement = $request->waistMeasurement;
-$neck = $request->neck;
-$height = $request->height;
-$hip = $request->hip;
 
 
-$waist_inch = $waist_measurement / 2.54;
-$neck_inch = $neck / 2.54;
-$hip_inch = $hip / 2.54;
-
-
-if ($gender === 'male') {
-    $body_density = 1.0324 - 0.19077 * log10($waist_inch - $neck_inch) + 0.15456 * log10($height);
-} elseif ($gender === 'female') {
-    $body_density = 1.29579 - 0.35004 * log10($waist_inch + $hip_inch - $neck_inch) + 0.221 * log10($height);
-} else {
-
-
-
-$bfp = (495 / $body_density) - 450;
 
 }
 

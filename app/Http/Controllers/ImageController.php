84361fe3fc9d00654cfacc4b051e344d812e1877
AxisCodee <raneemma934@gmail.com>
2024-04-01 10:24:46 +0300

    a

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -53,11 +53,7 @@ class ImageController extends Controller
     public function deleteUserImage(Request $request, $user)
     {
         try {
-<<<<<<< HEAD
             $result = $this->imageService->deleteUserImage($user, $request->type);
-=======
-            $result = $this->imageService->deleteoneImage($image);
->>>>>>> 0eb62a3326adba22549d8fcbb4214a323ae1b300
             return ResponseHelper::success($result);
         } catch (\Exception $e) {
             return ResponseHelper::error($e->getMessage(), $e->getCode());
@@ -73,7 +69,6 @@ class ImageController extends Controller
             return ResponseHelper::error($e->getMessage(), $e->getCode());
         }
     }
-<<<<<<< HEAD
     // public function deleteAllUserImage(User $user)
     // {
     //     try {
@@ -83,15 +78,13 @@ class ImageController extends Controller
     //         return ResponseHelper::error($e->getMessage(), $e->getCode());
     //     }
     // }
-=======
-    public function deleteAllUserImage(User $user)
+    public function deleteAllUserImage(Request $request, $user)
     {
         try {
-            $result = $this->imageService->deleteUserImages($user);
+            $result = $this->imageService->deleteUserImage($user, $request->type);
             return ResponseHelper::success($result);
         } catch (\Exception $e) {
             return ResponseHelper::error($e->getMessage(), $e->getCode());
         }
     }
->>>>>>> 0eb62a3326adba22549d8fcbb4214a323ae1b300
 }

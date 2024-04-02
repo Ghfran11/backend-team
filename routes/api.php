3992e77a28dbbea85894ca4f3dc9154ee72ea39a
thalesMworkAcc <thalesmwork942@gmail.com>
2024-04-02 15:33:14 +0300

    delete program

--- routes/api.php
+++ routes/api.php
@@ -69,6 +69,7 @@ Route::middleware('auth:api')->group(function () {
     Route::post('store', [ProgramController::class, 'store']);//Program
     Route::get('getCategory', [ProgramController::class, 'getCategory']);
     Route::post('updateprogram/{program}', [ProgramController::class, 'update']);
+    Route::get('deleteProgram/{program}', [ProgramController::class, 'destroy']);
     Route::post('asignprogram/{program}', [ProgramController::class, 'assignProgram']);
     Route::post('programCommitment', [ProgramController::class, 'programCommitment']);
     Route::get('downloadFile/{program}', [ProgramController::class, 'downloadFile']);
@@ -120,51 +121,51 @@ Route::middleware('auth:api')->group(function () {
     Route::get('status', [UserController::class, 'info']);
 
 
-Route::post('addOrder', [OrderController::class, 'store']);
-Route::post('updateOrrder/{order}', [OrderController::class, 'update']);
-Route::get('getMyOrder', [OrderController::class, 'getMyOrder']);
-Route::post('acceptOrder/{order}', [OrderController::class, 'acceptOrder']);
-Route::delete('deleteOrder', [OrderController::class, 'destroy']);
-Route::post('showOrder/{order}', [OrderController::class, 'show']);
-Route::get('showAnnual', [UserController::class, 'showAnnual']);
+    Route::post('addOrder', [OrderController::class, 'store']);
+    Route::post('updateOrrder/{order}', [OrderController::class, 'update']);
+    Route::get('getMyOrder', [OrderController::class, 'getMyOrder']);
+    Route::post('acceptOrder/{order}', [OrderController::class, 'acceptOrder']);
+    Route::delete('deleteOrder', [OrderController::class, 'destroy']);
+    Route::post('showOrder/{order}', [OrderController::class, 'show']);
+    Route::get('showAnnual', [UserController::class, 'showAnnual']);
 
-Route::post('requestPrograme', [OrderController::class, 'requestProgram']);
-Route::get('Premum', [OrderController::class, 'getPremium']);
-Route::get('myPlayer', [OrderController::class, 'showMyPlayer']);
-Route::get('myActivePlayer', [OrderController::class, 'myActivePlayer']);
-Route::post('cancle/{order}', [OrderController::class, 'cancelOrder']);
-Route::get('unAssign/{user}', [OrderController::class, 'unAssign']);
-Route::get('deletePlayer/{user}', [OrderController::class, 'deletePlayer']);
-Route::post('myPlayer', [OrderController::class, 'showMyPlayer']);
+    Route::post('requestPrograme', [OrderController::class, 'requestProgram']);
+    Route::get('Premum', [OrderController::class, 'getPremium']);
+    Route::get('myPlayer', [OrderController::class, 'showMyPlayer']);
+    Route::get('myActivePlayer', [OrderController::class, 'myActivePlayer']);
+    Route::post('cancle/{order}', [OrderController::class, 'cancelOrder']);
+    Route::get('unAssign/{user}', [OrderController::class, 'unAssign']);
+    Route::get('deletePlayer/{user}', [OrderController::class, 'deletePlayer']);
+    Route::post('myPlayer', [OrderController::class, 'showMyPlayer']);
 
 //user info
-Route::post('addInfo', [UserInfoController::class, 'store']);
-Route::post('updateInfo', [UserInfoController::class, 'update']);
-Route::get('showInfo/{user}', [UserInfoController::class, 'show']);
+    Route::post('addInfo', [UserInfoController::class, 'store']);
+    Route::post('updateInfo', [UserInfoController::class, 'update']);
+    Route::get('showInfo/{user}', [UserInfoController::class, 'show']);
 
 //monthly Subscription Avg
-Route::get('monSubsAvg', [SubscriptionController::class, 'monthlySubscriptionAvg']);
+    Route::get('monSubsAvg', [SubscriptionController::class, 'monthlySubscriptionAvg']);
 
 //finance
-Route::post('storeFinance', [InfoController::class, 'store']);
-Route::post('updateFinance/{info}', [InfoController::class, 'update']);
-Route::get('showFinance/{info}', [InfoController::class, 'show']);
-Route::get('subscriptions', [SubscriptionController::class, 'index']);
+    Route::post('storeFinance', [InfoController::class, 'store']);
+    Route::post('updateFinance/{info}', [InfoController::class, 'update']);
+    Route::get('showFinance/{info}', [InfoController::class, 'show']);
+    Route::get('subscriptions', [SubscriptionController::class, 'index']);
 
 //Article
-Route::post('addArticle', [ArticleController::class, 'store']);
-Route::get('allArticle', [ArticleController::class, 'index']);
-Route::delete('deleteArticle/{article}', [ArticleController::class, 'destroy']);
-Route::post('updateArticle/{id}', [ArticleController::class, 'update']);
-Route::get('myArticle', [ArticleController::class, 'getMyArticle']);
-Route::get('coachArticle/{user}', [ArticleController::class, 'getCoachArticle']);
-Route::post('makeFavourite/{article}', [ArticleController::class, 'makeFavourite']);
+    Route::post('addArticle', [ArticleController::class, 'store']);
+    Route::get('allArticle', [ArticleController::class, 'index']);
+    Route::delete('deleteArticle/{article}', [ArticleController::class, 'destroy']);
+    Route::post('updateArticle/{id}', [ArticleController::class, 'update']);
+    Route::get('myArticle', [ArticleController::class, 'getMyArticle']);
+    Route::get('coachArticle/{user}', [ArticleController::class, 'getCoachArticle']);
+    Route::post('makeFavourite/{article}', [ArticleController::class, 'makeFavourite']);
 
 
 //category
-Route::get('getCategories', [CategoryController::class, 'index']);
-Route::post('AddCategory', [CategoryController::class, 'store']);
-Route::get('getimage/{user}', [ImageController::class, 'getImages']);
+    Route::get('getCategories', [CategoryController::class, 'index']);
+    Route::post('AddCategory', [CategoryController::class, 'store']);
+    Route::get('getimage/{user}', [ImageController::class, 'getImages']);
 });
 
 

9d311aa7d48dbaf40ef1308fe8ef148fcc6da40b
AxisCodee <raneemma934@gmail.com>
2024-04-28 11:23:10 +0300

    aa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,17 +13,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UserSeeder::class);
-        $this->call(DaysSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(ProgramSeeder::class);
-        $this->call(ExerciseSeeder::class);
+       // $this->call(DaysSeeder::class);
+      //  $this->call(CategorySeeder::class);
+        //$this->call(ProgramSeeder::class);
+//$this->call(ExerciseSeeder::class);
        // $this->call(ImageSeeder::class);
-        $this->call(ReportSeeder::class);
-        $this->call(RatingSeeder::class);
-        $this->call(TimeSeeder::class);
-        $this->call(UserInfoSeeder::class);
-        $this->call(ArticleSeeder::class);
-        $this->call(InfoSeeder::class);
+      //  $this->call(ReportSeeder::class);
+      //  $this->call(RatingSeeder::class);
+       // $this->call(TimeSeeder::class);
+       // $this->call(UserInfoSeeder::class);
+      //  $this->call(ArticleSeeder::class);
+       // $this->call(InfoSeeder::class);
        // $this->call(OrderSeeder::class);
     }
 }

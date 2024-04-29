a8e61b614be5b9712d465b1927173da7ae27f4ac
AxisCodee <raneemma934@gmail.com>
2024-04-29 12:06:54 +0300

    a

--- app/Helper/files.php
+++ app/Helper/files.php
@@ -28,7 +28,7 @@ if (! function_exists('upload')) {
     function upload(UploadedFile $file, $filePath)
     {
         $fileName = time().'.'.$file->getClientOriginalExtension();
-        $path = $file->storeAs($filePath, $fileName, 'public/uploads/images');
+        $path = $file->storeAs($filePath, $fileName, 'public');
 
         return $path;
     }
